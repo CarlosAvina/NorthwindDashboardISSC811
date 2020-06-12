9e46d4ffde6124952b522e16c3fb8a85e30a3c2a
Carlos Avina <carlosavilu11@gmail.com>
2020-06-12 00:11:24 -0500

    mongo helper class

--- nodeapi/environments/env.production.ts
+++ nodeapi/environments/env.production.ts
@@ -3,5 +3,12 @@ export default {
         NAME: "API de autenticación",
         PORT: 3000,
         ENVIRONMENT: 'Development'
+    },
+    MONGODB: {
+        PORT: 27017,
+        HOST: 'localhost',
+        USER_NAME: '',
+        USER_PASSWORD: '',
+        DEFAULT_DATABASE: ''
     }
 }
---
+++ nodeapi/helpers/mongodb.helper.ts
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb';
+
+export default class MongoDBHelper {
+    public db: any;
+
+    private cnn: any;
+    private port: number;
+    private dbUri: string;
+    private static _instance: MongoDBHelper;
+
+    constructor(SETTINGS: any) {
+        this.port = SETTINGS.PORT;
+        this.dbUri = `mongodb://${SETTINGS.USER_NAME}:${SETTINGS.USER_PASSWORD}@${SETTINGS.HOST}/${SETTINGS.DEFAULT_DATABASE}`;
+    }
+
+    public static getInstance(settings: any) {
+        return this._instance || (this._instance = new this(settings));
+    }
+
+    async connect() {
+        await MongoClient.connect(this.dbUri, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+            (connection: any) => {
+                this.cnn = connection;
+                this.db = this.cnn.db();
+                console.log('Conexion a mongo db realizada de forma correcta');
+            }
+        ).catch((err: any) => {
+            console.log(`Ocurrio un error al intentar conectarse a la base de datos de MongoDB. La descripcion del error es la siguiente ${err}`);
+        })
+    }
+
+    setDatabase(dbName: string) {
+        this.db = this.cnn.db(dbName);
+    }
+
+    async close() {
+        await this.cnn.close();
+    }
+}
