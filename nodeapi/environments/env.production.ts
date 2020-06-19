556fd46544c54254312a56ac0ca6769aec04d4a4
Carlos Avina <carlosavilu11@gmail.com>
2020-06-18 22:33:16 -0500

    aws new ip

--- nodeapi/environments/env.production.ts
+++ nodeapi/environments/env.production.ts
@@ -6,7 +6,7 @@ export default {
     },
     MONGODB: {
         PORT: 27017,
-        HOST: 'ip-172-31-17-56.us-east-2.compute.internal',
+        HOST: 'ip-172-31-4-251.us-east-2.compute.internal',
         USER_NAME: 'dba-admin',
         USER_PASSWORD: 'mongoadmin123',
         DEFAULT_DATABASE: 'dbAuth'
