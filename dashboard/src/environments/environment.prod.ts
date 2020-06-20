21b2e2cd4db2838f5cb97eb65a54578480039f18
Carlos Avina <carlosavilu11@gmail.com>
2020-06-19 23:35:56 -0500

    correct protocol

--- dashboard/src/environments/environment.prod.ts
+++ dashboard/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   API: {
     EndPoint: {
-      Northwind: 'https://192.168.10.81/API.Northwind.ISSC811/v1/analysis/northwind/',
+      Northwind: 'http://192.168.10.81/API.Northwind.ISSC811/v1/analysis/northwind/',
       NODE: 'http://192.168.10.79:3000'
     }
   }
