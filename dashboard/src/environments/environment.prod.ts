a65323c4ff85276f9de4da3b716bbf32078c6ae5
Carlos Avina <carlosavilu11@gmail.com>
2020-06-20 19:59:52 -0500

    fixes

--- dashboard/src/environments/environment.prod.ts
+++ dashboard/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   API: {
     EndPoint: {
       Northwind: 'http://192.168.10.81/API.Northwind.ISSC811/v1/analysis/northwind/',
-      NODE: 'app.northwind.com'
+      NODE: 'node.northwind.com'
     }
   }
 };
