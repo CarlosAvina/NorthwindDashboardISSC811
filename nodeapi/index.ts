9666619dd34470993107ba33446602c2f3fe4b6f
Carlos Avina <carlosavilu11@gmail.com>
2020-06-22 17:35:13 -0500

    api cors

--- nodeapi/index.ts
+++ nodeapi/index.ts
@@ -19,6 +19,26 @@ app.use(express.json());
 
 app.use(cors({ origin: true, credentials: true }));
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://app.northwind.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/api/auth/test', (req: Request, res: Response) => {
     res.status(200).json({
         ok: true,
